703afd39ad36374d8f39d2627f085a555d116abc
tallyhawley <league@WTS-iMac-11.local>
2018-03-08 16:45:14 -0800

    20180308

--- src/intro_to_file_io/Runner.java
+++ src/intro_to_file_io/Runner.java
@@ -3,5 +3,9 @@ package intro_to_file_io;
 public class Runner {
 	public static void main(String[] args) {
 		ToDoList list = new ToDoList();
+		
+		String[] strarr = new String[4];
+		int length = strarr.length;
+		System.out.println(length);
 	}
 }
